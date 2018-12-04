81465c8e85b62adf2d7fd6d838755175027655a5
mj2905 <marcandreolivier@gmail.com>
2018-12-04 14:37:52 +0100

    orbit control

--- assets/js/src/map.js
+++ assets/js/src/map.js
@@ -2,22 +2,6 @@ if (WEBGL.isWebGLAvailable() === false) {
   document.body.appendChild(WEBGL.getWebGLErrorMessage());
 }
 
-/*
-const map = {
-  currentTime: null,
-  timeRange: null,
-  communities: [],
-
-  init: function() {},
-  seekTime: function(t) {
-    this.currentTime = t;
-  },
-  setTimeRange: function(dt) {
-    this.timeRange = dt;
-  },
-  updateCommunities: function(communities) {}
-};*/
-
 var pause_animation = false;
 
 let camera, controls, scene, renderer;
@@ -57,6 +41,8 @@ function seekTime(t) {
   textr.magFilter = THREE.NearestFilter;
   planeMaterial.map = textr;
   planeMaterial.needsUpdate = true;
+
+  generatePlaneHeightsBuffered();
 }
 
 
@@ -78,12 +64,11 @@ function init() {
   scene.background = new THREE.Color(0x333333);
 
   scene.fog = new THREE.FogExp2(0xcccccc, 0.001);
-  renderer = new THREE.WebGLRenderer(); //{ antialias: true } );
+  renderer = new THREE.WebGLRenderer({ antialias: true } );
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  //camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
   camera = new THREE.OrthographicCamera(
     window.innerWidth / -2,
     window.innerWidth / 2,
@@ -93,21 +78,14 @@ function init() {
     1500
   );
   camera.position.set(250, 120, 100);
-  //camera.zoom = 1.2;
-  //camera.updateProjectionMatrix();
+
   // controls
 
-  controls = new THREE.MapControls(camera, renderer.domElement);
-  //controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-  //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-
-  //controls.enablePan = false;
 
   controls.screenSpacePanning = false;
   controls.minDistance = 1;
-  //controls.maxDistance = 500;
 
   controls.minZoom = 1;
   controls.maxZoom = 10;
@@ -117,21 +95,11 @@ function init() {
   // world ***********************************************************************************************************
 
   planeGeometry = new THREE.PlaneBufferGeometry(1000, 1000, 200, 200);
-  //planeGeometry = new THREE.PlaneBufferGeometry(1000, 1000, 400, 400);
-
-  //let texture2 = new THREE.TextureLoader().load( "assets/img/rplace.png" );//new THREE.CanvasTexture( canvas);
-  //texture2.minFilter = THREE.NearestFilter;
-  //texture2.magFilter = THREE.NearestFilter;
-
   planeMaterial = new THREE.MeshPhongMaterial({
     color: 0xffffff,
     flatShading: true,
     specular: 0x0
   });
-  //let texture = new THREE.TextureLoader().load('assets/img/rplace.png');
-  //texture3.minFilter = THREE.LinearFilter;
-  //texture3.maxFilter = THREE.LinearFilter;
-  //let material3 = new THREE.MeshBasicMaterial( { map: texture3} );
 
   let planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
   planeMesh.rotateX(-Math.PI / 2);
@@ -155,21 +123,11 @@ function init() {
   scene.add(light);
   var light = new THREE.AmbientLight(0x222222);
   scene.add(light);
-  //
+
   window.addEventListener("resize", onWindowResize, false);
-
-  //}
-
-  //img.src = 'assets/img/rplace.png';
-  /*
-  var gui = new dat.GUI();
-  gui.add( controls, 'screenSpacePanning' );
-  */
 }
 
 function onWindowResize() {
-  //camera.aspect = window.innerWidth / window.innerHeight;
-
   camera.left = window.innerWidth / -2;
   camera.right = window.innerWidth / 2;
   camera.top = window.innerHeight / 2;
@@ -186,86 +144,22 @@ function animate() {
   render();
 
   if (!plane_images) return;
-
-  
-
-
-
-  //if(index_plane == 0) {
-  //if (index_plane % steps == 0) {
-  //  let index = index_plane / steps;
-  //  if (plane_images[index] && plane_images[(index + 1) % tot_images]) {
-  //    interpolator_images = d3.interpolateArray(
-  //      plane_images[index],
-  //      plane_images[(index + 1) % tot_images]
-  //    );
-  //  }
-  //}
-
-  /*
-  if (index_plane % (steps / 2) == 0) {
-    let index = (2 * index_plane) / steps;
-    let textr = new THREE.CanvasTexture(back_images[index]); //THREE.ImageUtils.loadTexture( src );
-    textr.minFilter = THREE.NearestFilter;
-    textr.magFilter = THREE.NearestFilter;
-    planeMaterial.map = textr;
-    planeMaterial.needsUpdate = true;
-  }
-
-  if (interpolator_images) {
-    generatePlaneHeightsBuffered();
-  }
-  */
-
-  //index_plane = (index_plane + 1) % (tot_images * steps); //}
-
-  generatePlaneHeightsBuffered();
 }
 
 function generatePlaneHeightsBuffered() {
   if (planeGeometry) {
     let positions = planeGeometry.attributes.position.array;
-    let arr = timeLevels.get(); //interpolator_images((index_plane % steps) / steps);
+    let arr = timeLevels.get();
 
-    //Top left is -500, 500
-    //Bottom right is 500, -500
-    ///!\ <=, not <
     for (let i = 0; i < filterSize; i++) {
       for (let j = 0; j < filterSize; j++) {
-        //let height = context.getImageData(i, j, 1, 1).data[0]; //red = blue = green
         positions[(i + j * (filterSize + 1)) * 3 + 2] = arr[i + j * filterSize];
       }
-      //planeGeometry.vertices[i].z = sinus(planeGeometry.vertices[i].x, planeGeometry.vertices[i].y) *10;
     }
 
     planeGeometry.attributes.position.needsUpdate = true;
   }
 }
-
-// function generatePlaneHeights(index) {
-//   if (planeGeometry) {
-//     let canvas = document.createElement("canvas");
-//     canvas.width = 200;
-//     canvas.height = 200;
-//     let context = canvas.getContext("2d");
-//     context.drawImage(plane_images[index], 0, 0);
-
-//     //Top left is -500, 500
-//     //Bottom right is 500, -500
-//     ///!\ <=, not <
-//     for (let i = 0; i < 200; ++i) {
-//       for (let j = 0; j < 200; ++j) {
-//         let height = context.getImageData(i, j, 1, 1).data[0]; //red = blue = green
-//         planeGeometry.vertices[i + j * 200].z = height;
-//       }
-//       //planeGeometry.vertices[i].z = sinus(planeGeometry.vertices[i].x, planeGeometry.vertices[i].y) *10;
-//     }
-
-//     planeGeometry.computeFaceNormals();
-//     planeGeometry.computeVertexNormals();
-//     planeGeometry.verticesNeedUpdate = true;
-//   }
-// }
 
 function render() {
   renderer.render(scene, camera);
